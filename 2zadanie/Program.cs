e0af65d47eb6bb3b4f380393aaaeb99234c6eb93
DiasKapassov <diasgoose@gmail.com>
2023-03-24 15:56:21 +0600

    Выполнил 2 задание

---
+++ 2zadanie/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Напишите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Напишите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Напишите число c: ");
+int c = int.Parse(Console.ReadLine());
+if (a>b) {
+    if (a>c) {
+        Console.Write("Число a больше всех");
+    }
+    else {
+        Console.Write("Число c больше всех");
+    }
+}
+else {
+    if (b > c) {
+    Console.Write("Число b больше всех");
+}
+else {
+    Console.Write("Число c больше всех");
+}
+}
