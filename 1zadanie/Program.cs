6a21e4f1f68ac5852420cbe9708a822f776d98e6
DiasKapassov <diasgoose@gmail.com>
2023-03-24 15:40:36 +0600

    Закончил 1 задание

---
+++ 1zadanie/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Напишите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Напишите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write ("Большее число: ");
+if (a > b) {
+Console.Write(a);
+}
+else {
+    Console.Write(b);
+}
