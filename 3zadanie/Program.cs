2b0bda8d4686b341a07a50f6680904d6afdd09c9
DiasKapassov <diasgoose@gmail.com>
2023-03-24 16:03:09 +0600

    Сделал 3 задание

---
+++ 3zadanie/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Напишите число: ");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0) {
+    Console.Write("Число четное");
+}
+else {
+    Console.Write("Число нечетное");
+}
+// int b = a%2;
+// Console.Write(b);
